5386f9ef27f7a5b3cb1a5225d523c4a6f84e97bd
sampurna09 <89682008+sampurna09@users.noreply.github.com>
2022-04-05 08:07:29 +0545

    Update Messages.jsx

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -50,3 +50,61 @@ class Messages extends Component {
 }
 
 export default Messages;
+
+
+
+
+import react from "react";
+import React from "react";
+import UserService from "../service/UserService";
+
+class UserComponent extends react.Component{
+
+constructor() { 
+    super();
+    this.state={
+        users:[]
+    }
+}
+
+componentDidMount(){
+    UserService.getUsers().then((response) => {
+        this.setState({users: response.data});
+    
+});
+}
+
+render(){
+    return(
+    <div>
+            <h1 className="text-center">UserList</h1>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <td>user_id</td>
+                        <td>first_name</td>
+                        <td>last_name</td>
+                        <td>phone_number</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.state.users.map(
+                            users =>
+                            <tr key ={users.id}>
+                                <td>{users.id}</td>
+                                <td>{users.firstName}</td>
+                                <td>{users.lastName}</td>
+                                <td>{users.phoneNumber}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+    </div>);
+}
+}
+
+
+
+export default UserComponent;
